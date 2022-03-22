1c102995a5aa997c7d02b1649de788acc2aa845d
Nicole Rosche <nicole.rosche@yahoo.com>
2022-03-22 17:10:54 +0800

    initial commit

---
+++ blackjack.js
@@ -0,0 +1,15 @@
+//Aim for a playable game. The essence of blackjack requires:
+//Two players - a player and a dealer (computer).
+//A deck of cards.
+//A starting hand of 2 cards for each player.
+//Comparing both hands and determining a winner. The possible scenarios are:
+//A tie. When both the player and dealer have the same total hand values - or if both draw Blackjack
+//A Blackjack win. When either player or dealer draw Blackjack.
+//A normal win. When neither draw Blackjack, the winner is decided by whomever has the higher hand total.
+
+
+
+
+function randomCard () {
+
+}
